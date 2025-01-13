a2bc782f8bc159c78ca77627dba339eba48e6c94
Ankit23232 <ankitprajapati2323@gmail.com>
2025-01-13 11:13:22 +0545

    Using getter and setter.

--- StudentManagementSystem/src/SMS/Student.java
+++ StudentManagementSystem/src/SMS/Student.java
@@ -1,5 +1,23 @@
 package SMS;
 
 public class Student {
-
+	String name;
+	int age;
+	
+	public Student(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+	public void setname(String name) {
+		this.name = name;
+	}
+	public void setnage(int age) {
+		this.age = age;
+	}
+	public void getname(String name) {
+		this.name = name;
+	}
+	public void getage(int age) {
+		this.age = age;
+	}
 }
